5ffe2e81f1783fff32d14617efcdd5dce562966b
ko-tarou <akokoa1221@gmail.com>
2024-11-01 13:37:51 +0900

    共有開始

--- src/pages/Toppage.js
+++ src/pages/Toppage.js
@@ -7,6 +7,11 @@ function Toppage() {
   
   return (
     <div className='Toppage'>
+      <div>高岡己太朗</div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
     </div>
   )
 }
