d99a24c748c5ba26250a83f0030094e131550ad9
ko-tarou <akokoa1221@gmail.com>
2024-11-01 13:33:37 +0900

    環境構築終了

--- src/pages/Toppage.js
+++ src/pages/Toppage.js
@@ -4,58 +4,9 @@ import "../App.css"
 
 function Toppage() {
 
-  const circle = {
-    width: "50px",
-    height: "50px",
-    backgroundColor: "#000000",
-    borderRadius:"50%"
-  }
-
-  const stick = {
-    width:"10px",
-    height: "100px",
-    backgroundColor: "#000000"
-  }
-
-
+  
   return (
     <div className='Toppage'>
-
-      
-      <div className='body'>
-        <div className='main'>
-          <div className='title'>
-            ようこそ！
-          </div>
-        </div>
-
-        <div className='activity'>
-          <div className='title'>主な活動</div>
-          <div className='info'>
-            <div className='box'>
-              <div className='smart-project'>スマプロ</div>
-            </div>
-            <div className='box'>
-              <div className='busstop-project'>BusStop</div>
-            </div>
-            <div className='box'>
-              
-            </div>
-          </div>
-        </div>
-
-        <div style={circle}></div>
-        <div style={stick}></div>
-        <div style={circle}></div>
-        <div style={stick}></div>
-        <div style={circle}></div>
-        <div style={stick}></div>
-        <div style={circle}></div>
-        <div style={stick}></div>
-        <div style={circle}></div>
-        <div style={stick}></div>
-        <div style={circle}></div>
-      </div>
     </div>
   )
 }
