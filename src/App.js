b5d05a92c86f90a0965e3e8435218ba3820cd582
ko-tarou <akokoa1221@gmail.com>
2024-11-02 17:35:04 +0900

    ヘッダーの高さを指定

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { BrowserRouter as Router, Route, Routes,Link } from "react-router-dom";
 import Toppage from "./pages/Toppage"
 import Projectpage from "./pages/Projectpage.js"
+import SlidePage from "./pages/Slidepage.js"
 
 function App() {
   return (
@@ -10,11 +11,13 @@ function App() {
         <nav>
           <Link to="/">ホーム</Link>
           <Link to="/project">プロジェクト</Link>
+          <Link to="/slide">スライド</Link>
         </nav>
       </header>
       <Routes>
         <Route path="/" element={<Toppage/>}/>
         <Route path="/project" element={<Projectpage/>}/>
+        <Route path="/slide" element={<SlidePage/>}/>
       </Routes>
     </Router>
   );
