20f3288f0939d891b7a81bdabf0635672e17d26d
ko-tarou <akokoa1221@gmail.com>
2024-11-03 23:55:05 +0900

    タブのcssを調整した

--- src/common/object-list-tab/TabContent.js
+++ src/common/object-list-tab/TabContent.js
@@ -2,10 +2,12 @@ import React from 'react';
 
 function TabContent({ activeTab }) {
   return (
-    <div style={{ border: '1px solid gray', padding: '20px' }}>
-      {activeTab === "tab1" && <div>タブ1の内容</div>}
+    <div style={{padding: '20px' }}>
+      {activeTab === "tab1" && <div className='Tabs-content'>タブ1の内容</div>}
       {activeTab === "tab2" && <div>タブ2の内容</div>}
       {activeTab === "tab3" && <div>タブ3の内容</div>}
+      {activeTab === "tab4" && <div>タブ4の内容</div>}
+      {activeTab === "tab5" && <div>タブ5の内容</div>}
     </div>
   );
 }
