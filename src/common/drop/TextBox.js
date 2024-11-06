97bc31155b0445b10842af026013e2e4a523df6b
ko-tarou <akokoa1221@gmail.com>
2024-11-06 15:47:15 +0900

    テキストボックスにした

--- src/common/drop/TextBox.js
+++ src/common/drop/TextBox.js
@@ -1,28 +1,46 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useDrag } from 'react-dnd';
 import { ItemTypes } from './ItemTypes';
 
-function TextBox({ text }) {
+function TextBox({ id, text, x, y, onTextChange }) {
+  const [value, setValue] = useState(text || "");
+
+  useEffect(() => {
+    setValue(text);
+  }, [text]);
+
   const [{ isDragging }, drag] = useDrag({
     type: ItemTypes.TEXT_BOX,
-    item: { text },
+    item: { id, text: value, x, y },
     collect: (monitor) => ({
       isDragging: monitor.isDragging(),
     }),
   });
 
+  const handleChange = (e) => {
+    setValue(e.target.value);
+    onTextChange(id, e.target.value);
+  };
+
   return (
-    <div
+    <input
       ref={drag}
+      type="text"
+      value={value}
+      onChange={handleChange}
       style={{
+        width: '20px',
+        height: '20px',
         opacity: isDragging ? 0.5 : 1,
         padding: '10px',
         border: '1px solid black',
         cursor: 'move',
+        position: 'absolute',
+        left: x,
+        top: y,
+        transform: 'translate(-50%, -50%)', // 中央揃えの調整
       }}
-    >
-      {text}
-    </div>
+    />
   );
 }
 
--- src/pages/tabs/Char.js
+++ src/pages/tabs/Char.js
@@ -1,16 +1,13 @@
 import React from 'react'
-import MovableTextBox from '../../common/MovableTextBox'
 import "../../styles/tabs/Char.css"
 import TextBoxArea from "../../common/drop/TextBoxArea"
 
 function Char() {
   return (
     <div className='Char'>
-      <div className='normal'>テキスト</div>
       <div className='item'>
-        <MovableTextBox/>
+        <TextBoxArea/>
       </div>
-      <TextBoxArea/>
     </div>
 	)
 }
