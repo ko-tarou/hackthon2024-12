31765804ffd69608335204ee3e5b977891b8b169
nokonoko2525 <c1407448@st.kanazawa-it.ac.jp>
2024-11-15 16:27:58 +0900

    クリックした際に枠線が強調されるboxのコンポーネント作成

---
+++ src/pages/SlideList/SlideList.js
@@ -0,0 +1,24 @@
+import React, { useRef } from 'react';
+import './SlideList.css';
+
+const SlideList = () => {
+    const textRef = useRef(null);
+
+  const handleClick = () => {
+    if (textRef.current) {
+      textRef.current.focus(); // クリック時にフォーカスを設定
+    }
+  };
+
+  return (
+    <div
+      ref={textRef}
+      tabIndex={0}  // フォーカス可能にするために tabIndex を追加
+      className="clickable-text"
+      onClick={handleClick}
+    >
+    </div>
+  );
+};
+
+export default SlideList;
