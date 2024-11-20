349b5bb6334158d3fc04a096f81031bd75db7fae
karir <c1406274@st.kanazawa-it.ac.jp>
2024-11-20 10:11:17 +0900

    ？ボタンの説明書を表示させたz

--- src/App.js
+++ src/App.js
@@ -1,13 +1,34 @@
 import './App.css';
 import { BrowserRouter as Router, Route, Routes, Link, useLocation } from "react-router-dom";
+import React, { useState } from "react";
 import Toppage from "./pages/TopPage/TopPage.js";
 import Projectpage from "./pages/ProjectPage/ProjectPage.js";
 import SlidePage from "./pages/SlidePage/SlidePage.js";
 import TestPage from "./pages/TestPage/TestPage.js";
 import SlideView from "./pages/SlideView/SlideView.js";
 
+const Modal = ({ isOpen, onClose }) => {
+  if (!isOpen) return null;
+
+  return (
+    <div className="modal-overlay" onClick={onClose}>
+      <div className="modal-content" onClick={(e) => e.stopPropagation()}>
+        <button className="modal-close-button" onClick={onClose}>
+          ×
+        </button>
+        <h2>説明書</h2>
+        <p>使い方ここに書く（行によって×ボタンの位置変更いる）</p>
+      </div>
+    </div>
+  );
+};
+
 const Header = () => {
   const location = useLocation();
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const openModal = () => setIsModalOpen(true);
+  const closeModal = () => setIsModalOpen(false);
 
   return (
     <header>
@@ -16,7 +37,6 @@ const Header = () => {
         <Link to="/project">プロジェクト</Link>
         <Link to="/slide">スライド</Link>
         <Link to="/test">実験</Link>
-        {/* 条件に応じて検索フィールドと検索ボタンを表示 */}
         {['/', '/slide'].includes(location.pathname) && (
           <div className="search-container">
             <input
@@ -28,7 +48,8 @@ const Header = () => {
           </div>
         )}
       </nav>
-      <button className="circle-button">?</button>
+      <button className="circle-button" onClick={openModal}>?</button>
+      <Modal isOpen={isModalOpen} onClose={closeModal} />
     </header>
   );
 };
@@ -36,16 +57,13 @@ const Header = () => {
 function App() {
   return (
     <Router>
-      {/* ヘッダーコンポーネント */}
       <Header />
-      
-      {/* ルートの設定 */}
       <Routes>
-        <Route path="/" element={<Toppage/>}/>
-        <Route path="/project" element={<Projectpage/>}/>
-        <Route path="/slide" element={<SlidePage/>}/>
-        <Route path="/test" element={<TestPage/>}/>
-        <Route path="/slideview" element={<SlideView/>}/>
+        <Route path="/" element={<Toppage />} />
+        <Route path="/project" element={<Projectpage />} />
+        <Route path="/slide" element={<SlidePage />} />
+        <Route path="/test" element={<TestPage />} />
+        <Route path="/slideview" element={<SlideView />} />
       </Routes>
     </Router>
   );
